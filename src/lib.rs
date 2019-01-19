7a05efff4748512a6d0efa4eccc42676678f914d
Pascal Hartig <phartig@rdrei.net>
2019-01-19 14:15:25 +0000

    Implement horribly imperative tick

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,73 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+#[derive(Debug, PartialEq, Eq)]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        // TODO: Meh, this should return a new Universe instead of mutating itself.
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for column in 0..self.width {
+                let idx = self.get_index(row, column);
+                let cell = self.cells[idx];
+                let live_neighbor_count = self.live_neighbor_count(row, column);
+
+                let next_cell = match (cell, live_neighbor_count) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+
+                };
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        return (row * self.width + column) as usize;
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        // TODO: This should be a sum over a flattened fold.
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_column == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_column = (column + delta_column) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_column);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
 extern {
     fn alert(s: &str);
 }
